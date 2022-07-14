37976f22d04711e519fc5a8df08ad28fa982981c
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-07-14 17:32:04 -0300

    Concluido o requisito 4

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [desabilitaBtn, setDesabilitaBtn] = useState(true);
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  useEffect(() => {
+    const emailValido = /\S+@\S+\.\S+/.test(email);
+    const minimoDeCaracterers = 6;
+    if (senha.length > minimoDeCaracterers && emailValido) {
+      setDesabilitaBtn(false);
+    } else {
+      setDesabilitaBtn(true);
+    }
+  }, [email, senha]);
+
   return (
     <div>
       <input
         type="email"
         placeholder="email"
         data-testid="email-input"
+        onChange={ ({ target }) => setEmail(target.value) }
       />
       <input
         type="password"
         placeholder="password"
         data-testid="password-input"
+        onChange={ ({ target }) => setSenha(target.value) }
       />
       <button
         type="button"
         data-testid="login-submit-btn"
+        disabled={ desabilitaBtn }
       >
         Enter
       </button>
