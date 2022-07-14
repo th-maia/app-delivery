cba6b00d84240d1f96a2987c3e97eb885fb97f0c
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-07-14 18:16:16 -0300

    Concluido o requisito 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Login from './components/Login';
+import RecipeProvider from './context/RecipeProvider';
 
 function App() {
   return (
-    <Login />
+    <RecipeProvider>
+      <Login />
+    </RecipeProvider>
   );
 }
 
