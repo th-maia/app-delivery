a71b075a21ed2c4cd374b6b9b4d808e4b1a36d91
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-07-15 15:30:53 -0300

    Concluidos os requisitos 7 e 8

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import Foods from './components/Foods';
+import Login from './pages/Login';
+import Foods from './pages/Foods';
+import Drinks from './pages/Drinks';
+import Profile from './pages/Profile';
+import DoneRecipe from './pages/DoneRecipe';
+import FavoriteRecipe from './pages/FavoriteRecipe';
 import RecipeProvider from './context/RecipeProvider';
 
 function App() {
@@ -11,7 +15,17 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Login } />
-          <Route path="/foods" component={ Foods } />
+          <Route exact path="/foods" component={ Foods } />
+          <Route exact path="/drinks" component={ Drinks } />
+
+          <Route exact path="/foods/:id-da-receita" component={ Foods } />
+          <Route exact path="/drinks/:id-da-receita" component={ Drinks } />
+          <Route exact path="/foods/:id-da-receita/in-progress" component={ Foods } />
+          <Route exact path="/drinks/:id-da-receita/in-progress" component={ Drinks } />
+
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ DoneRecipe } />
+          <Route exact path="/favorite-recipes" component={ FavoriteRecipe } />
         </Switch>
       </BrowserRouter>
     </RecipeProvider>
